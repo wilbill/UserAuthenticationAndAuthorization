f9bf7a41ef69df86511fb2c92bb2d8c2d4d9f750
wilbill <ntabazibilly57@gmail.com>
2024-01-04 14:26:17 -0600

    initial commit

---
+++ src/main/java/com/security/userauthenticationandauthorization/UserAuthenticationAndAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.security.userauthenticationandauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationAndAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationAndAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/userauthenticationandauthorization/UserAuthenticationAndAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.userauthenticationandauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationAndAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
